ec9d5a468993628b02f677d5cefba25bcfa1dc22
chinelon <72803021+chinelon@users.noreply.github.com>
2023-12-11 10:25:04 +0100

    latest commit

--- index.js
+++ index.js
@@ -25,3 +25,24 @@ const missKent = (function(){
 })();
 
 console.log(missKent.issuereports);
+
+//getters and setters
+
+let user = {
+    name: "John",
+    surname: "Smith",
+  
+    get fullName() {
+      return `${this.name} ${this.surname}`;
+    },
+  
+    set fullName(value) {
+      [this.name, this.surname] = value.split(" ");
+    }
+  };
+  
+  // set fullName is executed with the given value.
+  user.fullName = "Alice Cooper";
+  
+  alert(user.name); // Alice
+  alert(user.surname); // Cooper
