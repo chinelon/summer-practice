6408e1465d55bdc6ee4b8c1ae2cb4cdcfedee460
chinelon <72803021+chinelon@users.noreply.github.com>
2024-01-23 08:06:45 +0100

    apis

--- javascript/callbacks.js
+++ javascript/callbacks.js
@@ -30,3 +30,14 @@ fetch('https://url.com/some/url')
   fetch('url.url.com/api', {
     mode: 'cors'
   });
+
+  //error handling in async
+  async function getPersonsInfo(name) {
+    try {
+      const people = await server.getPeople();
+      const person = people.find(person => { return person.name === name });
+      return person;
+    } catch (error) {
+      // Handle the error any way you'd like
+    }
+  }
