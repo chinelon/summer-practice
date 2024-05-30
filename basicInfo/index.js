b990cfbb0c2d0d5013dd0b64fa2ed0d43d54a3fb
chinelon <72803021+chinelon@users.noreply.github.com>
2024-05-30 14:46:51 +0100

    node assignment completed

--- basicInfo/index.js
+++ basicInfo/index.js
@@ -1,9 +1,28 @@
+
 var http = require('http');
+var url = require('url');
 var fs = require('fs');
+
 http.createServer(function (req, res) {
-  fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8081);
+    var q = url.parse(req.url, true);
+    var filename = q.pathname;
+    var path;
+
+    if (filename === '/index.html' || filename ==='/') {
+        path = './index.html'
+    } else if ( filename === '/about.html'){
+        path = './about.html'
+    } else if ( filename === '/contact-me.html') {
+        path = './contact-me.html'
+    } else path = './404.html'
+
+    fs.readFile(path, function (err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
--- node/myfirst.js
+++ node/myfirst.js
@@ -1,8 +1,26 @@
-var http = require('http');
-var dt = require('./myfirstmod');
+// var http = require('http');
+// var dt = require('./myfirstmod');
 
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.write("The date and time are currently: " + dt.myDateTime());
+//   res.end();
+// }).listen(8080);
+
+var http = require('http');
+var uc = require('upper-case');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime());
+  res.write(uc.upperCase("Hello World!"));
   res.end();
-}).listen(8080);
+}).listen(8081);
+
+// var http = require('http');
+// var fs = require('fs');
+// http.createServer(function (req, res) {
+//   fs.readFile('index.html', function(err, data) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write(data);
+//     return res.end();
+//   });
+// }).listen(8081);
