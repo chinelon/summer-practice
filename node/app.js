83bb6776a30b22f2763c61bb45d4ad6f0c35bec0
chinelon <72803021+chinelon@users.noreply.github.com>
2024-05-30 14:13:47 +0100

    node

---
+++ node/app.js
@@ -0,0 +1,85 @@
+process.env.PORT; // "3000"
+//GET Request with Axios
+const axios = require('axios');
+
+axios
+  .get('https://example.com/todos')
+  .then(res => {
+    console.log(`statusCode: ${res.status}`);
+    console.log(res);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+//GET Request with Node standard modules
+const https = require('https');
+
+const options = {
+  hostname: 'example.com',
+  port: 443,
+  path: '/todos',
+  method: 'GET',
+};
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on('data', d => {
+    process.stdout.write(d);
+  });
+});
+
+req.on('error', error => {
+  console.error(error);
+});
+
+req.end();
+
+//POST Request with Axios
+const axios = require('axios');
+
+axios
+  .post('https://whatever.com/todos', {
+    todo: 'Buy the milk',
+  })
+  .then(res => {
+    console.log(`statusCode: ${res.status}`);
+    console.log(res);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+  
+//POST Request with Node standard modules
+const https = require('https');
+
+const data = JSON.stringify({
+  todo: 'Buy the milk',
+});
+
+const options = {
+  hostname: 'whatever.com',
+  port: 443,
+  path: '/todos',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length,
+  },
+};
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on('data', d => {
+    process.stdout.write(d);
+  });
+});
+
+req.on('error', error => {
+  console.error(error);
+});
+
+req.write(data);
+req.end();
